d980df29b7ed53ea0261890eb0085a57885b15ad
M1 <25163821+m1fnbr@users.noreply.github.com>
2022-07-23 14:36:04 -0400

    Added AOB Scanning(Future Proofing)

--- VoiceMeeterTest/dllmain.cpp
+++ VoiceMeeterTest/dllmain.cpp
@@ -1,49 +1,43 @@
+#pragma once
 #include "pch.h"
 #include <iostream>
 #include "templates.h"
+#include "mem.h"
+
 
 
 // Make sure DLL name is Mfplat.dll;
 
 
-uintptr_t BaseAddress;
-bool debug = false;
+bool debug = true;
+unsigned long RefProt;
+iterable_queue<uintptr_t> arr;
 
 
-DWORD WINAPI PatchThread(LPVOID) {
-   unsigned long RefProt;
-
-   iterable_queue<uintptr_t> arr;
-
-    arr.push((uintptr_t)BaseAddress + 0xD680A);
-    arr.push((uintptr_t)BaseAddress + 0xD5D5D);
-    arr.push((uintptr_t)BaseAddress + 0xD5D66);
-    arr.push((uintptr_t)BaseAddress + 0xD66CA);
-    arr.push((uintptr_t)BaseAddress + 0xD66D7);
 
 
+DWORD WINAPI PatchThread(LPVOID) 
+{
+    arr.push((uintptr_t)mem::FindPattern("83 3D 9B 00 20 00 00"));
+    arr.push((uintptr_t)mem::FindPattern("44 39 2D 48 0B 20 00"));
+    arr.push((uintptr_t)mem::FindPattern("44 39 2D 43 0B 20 00"));
+    arr.push((uintptr_t)mem::FindPattern("44 39 2D DB 01 20 00"));
+    arr.push((uintptr_t)mem::FindPattern("44 39 2D D2 01 20 00"));
     for (auto bytarrnum = arr.begin(); bytarrnum != arr.end(); ++bytarrnum)
     {
         std::cout << "Patched:  " << "0x" << std::hex << *bytarrnum << std::endl;
         char byts[] = "\x83\x3D\x9B\x00\x20\x00\x01"; // cmp DWORD PTR [rip+0x20009b],0x1
-
-
         // Turns out it was protected (changed entitlements and then setting the old entitlements back after the patch.)
         VirtualProtect((LPVOID)((uintptr_t)*bytarrnum), 7, PAGE_EXECUTE_READWRITE, &RefProt);
         memcpy((LPVOID)*bytarrnum, byts, 7);
         VirtualProtect((LPVOID)(*bytarrnum), 7, RefProt, NULL);
     }
-
     // Applies Patch which makes it seem like you are activated already when you try to activate.
-
-
-    uintptr_t CongAddy = BaseAddress + 0x10E71;
+    uintptr_t CongAddy = mem::FindPattern("83 B8 F0 0C 00 00 01");
     char congbts[] = "\x83\xB8\xF0\x0C\x00\x00\x00"; //cmp DWORD PTR [rax+0xcf0],0x0
-
     VirtualProtect((LPVOID)((uintptr_t)CongAddy), 7, PAGE_EXECUTE_READWRITE, &RefProt);
     memcpy((LPVOID)(uintptr_t)CongAddy, congbts, 7);
     VirtualProtect((LPVOID)(CongAddy), 7, RefProt, NULL);
-
     return 0;
 }
 
@@ -56,8 +50,6 @@ DWORD WINAPI MainThread(LPVOID)
         FILE* pFile;
         freopen_s(&pFile, "CONOUT$", "w", stdout);
     }
-    BaseAddress = reinterpret_cast<uintptr_t>(GetModuleHandle(0));
-
     CreateThread(0, 0, PatchThread, 0, 0, 0);
     return 0;
 }
---
+++ VoiceMeeterTest/mem.h
@@ -0,0 +1,103 @@
+#pragma once
+#include "pch.h"
+#include <iostream>
+#include <Psapi.h>
+#include <vector>
+#include <winscard.h>
+
+
+namespace mem {
+
+
+	static __forceinline uintptr_t FindPattern(const char* signature, bool bRelative = false, uint32_t offset = 0)
+	{
+		uintptr_t base_address = reinterpret_cast<uintptr_t>(GetModuleHandle(NULL));
+		static auto patternToByte = [](const char* pattern)
+		{
+			auto bytes = std::vector<int>{};
+			const auto start = const_cast<char*>(pattern);
+			const auto end = const_cast<char*>(pattern) + strlen(pattern);
+
+			for (auto current = start; current < end; ++current)
+			{
+				if (*current == '?')
+				{
+					++current;
+					if (*current == '?') ++current;
+					bytes.push_back(-1);
+				}
+				else { bytes.push_back(strtoul(current, &current, 16)); }
+			}
+			return bytes;
+		};
+
+		const auto dosHeader = (PIMAGE_DOS_HEADER)base_address;
+		const auto ntHeaders = (PIMAGE_NT_HEADERS)((std::uint8_t*)base_address + dosHeader->e_lfanew);
+
+		const auto sizeOfImage = ntHeaders->OptionalHeader.SizeOfImage;
+		auto patternBytes = patternToByte(signature);
+		const auto scanBytes = reinterpret_cast<std::uint8_t*>(base_address);
+
+		const auto s = patternBytes.size();
+		const auto d = patternBytes.data();
+
+		for (auto i = 0ul; i < sizeOfImage - s; ++i)
+		{
+			bool found = true;
+			for (auto j = 0ul; j < s; ++j)
+			{
+				if (scanBytes[i + j] != d[j] && d[j] != -1)
+				{
+					found = false;
+					break;
+				}
+			}
+			if (found)
+			{
+				uintptr_t address = reinterpret_cast<uintptr_t>(&scanBytes[i]);
+				if (bRelative)
+				{
+					address = ((address + offset + 4) + *(int32_t*)(address + offset));
+					return address;
+				}
+				return address;
+			}
+		}
+		return NULL;
+	}
+
+	static inline BOOL MaskCompare(PVOID pBuffer, LPCSTR lpPattern, LPCSTR lpMask)
+	{
+		for (auto value = reinterpret_cast<PBYTE>(pBuffer); *lpMask; ++lpPattern, ++lpMask, ++value)
+		{
+			if (*lpMask == 'x' && *reinterpret_cast<LPCBYTE>(lpPattern) != *value)
+				return false;
+		}
+
+		return true;
+	}
+
+	static inline PVOID FindPattern(PVOID pBase, DWORD dwSize, LPCSTR lpPattern, LPCSTR lpMask)
+	{
+		dwSize -= static_cast<DWORD>(strlen(lpMask));
+
+		for (auto index = 0UL; index < dwSize; ++index)
+		{
+			auto pAddress = reinterpret_cast<PBYTE>(pBase) + index;
+
+			if (MaskCompare(pAddress, lpPattern, lpMask))
+				return pAddress;
+		}
+
+		return NULL;
+	}
+
+	static inline PVOID FindPattern(LPCSTR lpPattern, LPCSTR lpMask)
+	{
+		MODULEINFO info = { 0 };
+
+		GetModuleInformation(GetCurrentProcess(), GetModuleHandle(0), &info, sizeof(info));
+
+		return FindPattern(info.lpBaseOfDll, info.SizeOfImage, lpPattern, lpMask);
+	}
+}
